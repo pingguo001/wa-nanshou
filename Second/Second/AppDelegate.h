d2eabd67af779f82e4991d5b9f0fc805ab73d8fb
pingguo001 <30429186+pingguo001@users.noreply.github.com>
2017-07-26 17:20:27 +0800

    初始化项目

---
+++ Second/Second/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Second
+//
+//  Created by 王海玉 on 2017/7/26.
+//  Copyright © 2017年 王海玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Second/Second/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Second
+//
+//  Created by 王海玉 on 2017/7/26.
+//  Copyright © 2017年 王海玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
